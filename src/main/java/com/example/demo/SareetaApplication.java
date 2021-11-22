08f4bec6912446a8247c88ad2c768719ce9ddfa7
shahidaarbi <munni222@gmail.com>
2021-11-21 22:02:34 -0500

    init commit

---
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableJpaRepositories("com.example.demo.model.persistence.repositories")
+@EntityScan("com.example.demo.model.persistence")
+@SpringBootApplication(exclude=(SecurityAutoConfiguration.class))
+public class SareetaApplication {
+
+	static final Logger logger = LoggerFactory.getLogger(SareetaApplication.class);
+
+	
+	  @Bean public BCryptPasswordEncoder bCryptPasswordEncoder(){ return new
+	  BCryptPasswordEncoder(); }
+	 
+	 
+	public static void main(String[] args) {
+		SpringApplication.run(SareetaApplication.class, args);
+		logger.info("Log file Start");
+	}
+
+}
---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,76 @@
+package com.example.demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.SareetaApplication;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import com.example.demo.security.UserDetailsServiceImpl;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private CartRepository cartRepository;
+	static final Logger logger = LoggerFactory.getLogger(SareetaApplication.class);
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	  @Autowired private UserDetailsServiceImpl userDetailsService;
+	 
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<User> test(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		User user = new User();
+		user.setUsername(createUserRequest.getUsername());
+		Cart cart = new Cart();
+		cartRepository.save(cart);
+		user.setCart(cart);
+		if(createUserRequest.getPassword().length()<7 ||
+				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+		    logger.error("CreateUser request failures");
+			return ResponseEntity.badRequest().build();
+		}
+		
+		
+		user.setSalt(userDetailsService.createSalt());
+
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		//user.setPassword(createUserRequest.getPassword());
+		userRepository.save(user);
+		logger.info("CreateUser request successes");
+		return ResponseEntity.ok(user);
+	}
+}
